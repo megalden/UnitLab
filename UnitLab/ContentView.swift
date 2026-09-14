344ca22406204f4eb5937d0b89f6c84ff32d728e
Galyshka <volkrokvolk@gmail.com>
2026-09-14 10:40:13 +0300

    Initial Commit

---
+++ UnitLab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UnitLab
+//
+//  Created by Володимир Галушка on 14.09.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UnitLab/UnitLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitLabApp.swift
+//  UnitLab
+//
+//  Created by Володимир Галушка on 14.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct UnitLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
